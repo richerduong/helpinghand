c87d7f1023370d8d9fd10bee1c18a4e073a623ab
j-uzr <jd.umzr@gmail.com>
2024-09-18 18:32:14 -0500

    feat: Created template for volunteer matching page

--- app/events/matching/page.tsx
+++ app/events/matching/page.tsx
@@ -1,6 +1,8 @@
 // import { auth } from '@/lib/auth';
 // import { redirect } from 'next/navigation';
 
+import Match from "./Match";
+
 export default async function VolunteerMatching() {
   // const session = await auth();
 
@@ -12,7 +14,8 @@ export default async function VolunteerMatching() {
     <>
       <div className="max-w-1170 w-full mx-auto flex justify-center h-full">
         <div className="bg-white border-2 border-[#C5C9D6] mt-4 rounded-2xl p-12 mb-4 w-4/5">
-          <p>Hi Jorge, enter volunteer matching component here</p>
+          {/* <VolunteerMatching session={session} /> */}
+          <Match />
           <hr className="border-gray-300 w-full my-4 mb-6" />
         </div>
       </div>
