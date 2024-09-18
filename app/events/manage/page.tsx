93cbc2995e0eb920dcc64c8f2a31e62e9d2494af
j-uzr <jd.umzr@gmail.com>
2024-09-18 17:21:04 -0500

    feat: Implemented Event Management Page

--- app/events/manage/page.tsx
+++ app/events/manage/page.tsx
@@ -1,6 +1,8 @@
 // import { auth } from '@/lib/auth';
 // import { redirect } from 'next/navigation';
 
+import Manage from "./Manage";
+
 export default async function EventManagement() {
   // const session = await auth();
 
@@ -12,7 +14,8 @@ export default async function EventManagement() {
     <>
       <div className="max-w-1170 w-full mx-auto flex justify-center h-full">
         <div className="bg-white border-2 border-[#C5C9D6] mt-4 rounded-2xl p-12 mb-4 w-4/5">
-          <p>Hi Jorge, enter event management component here</p>
+          {/* <EventManagement session={session} /> */}
+          <Manage />
           <hr className="border-gray-300 w-full my-4 mb-6" />
         </div>
       </div>
